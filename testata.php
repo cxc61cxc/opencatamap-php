053157eeb12c17bec479ee25e5b6c79a29db79dd
cxc61cxc <t4950c@gmail.com>
2017-12-16 15:37:01 +0100

    Update testata.php

--- testata.php
+++ testata.php
@@ -110,9 +110,9 @@ $db->close();
 	<?php
 	print "<p><a href=http://sister.agenziaentrate.gov.it/Main/index.jsp><small>" . $dt . "</p><p>" . $df . "</small></a></p>";
 	?>
-	/* eventuale dicitura a sinistra
+	<!-- eventuale dicitura a sinistra
 	<p><br><br>pinco pallo</p>
-	*/
+	-->
 	</td>
 	<td style="text-align: center; width: 20%">
 	<?php
@@ -128,9 +128,9 @@ print "COMUNE di " . $com;
 	echo '
 <p>  Marco Braida - Carlo A. Nicolini</p>
 <p><a href="http://giscarrara/ubucomu/logout.php">logout</a></p>
-/* eventuale dicitura a destra
+<!-- eventuale dicitura a destra
 <p><br><br>U.O. Progettazione e gestione PRG</p>
-*/
+-->
 ';
 	?>
 	</td>
